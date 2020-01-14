2c3cabc30fa1bbab848cd7067da1db1c57998269
Amador Cuenca <sphi02ac@gmail.com>
2020-01-14 19:12:48 -0300

    feat: Initialize the Flask application

---
+++ todo_api/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+
+def create_app():
+    """
+    Create a Flask application using the factory pattern
+
+    :return: Flask application instance
+    """
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return "Hello World"
+
+    return app
+
+
+app = create_app()
+app.run(debug=True)
