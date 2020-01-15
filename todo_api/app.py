e0e67e1e5858f86f1b8c63600e270178174e1775
Amador Cuenca <sphi02ac@gmail.com>
2020-01-15 12:38:53 -0300

    fix: Create instance relative settings

--- todo_api/app.py
+++ todo_api/app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 
 
-def create_app():
+def create_app(settings_override=None):
     """
     Create a Flask application using the factory pattern
 
     :return: Flask application instance
     """
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object('config.settings')
+    app.config.from_pyfile('config.py', silent=True)
+
+    if settings_override:
+        app.config.update(settings_override)
 
     register_extensions(app)
 
