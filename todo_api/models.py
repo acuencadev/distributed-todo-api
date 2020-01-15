ee15d1838ef03813610565e2967189d1f2d6a241
Amador Cuenca <sphi02ac@gmail.com>
2020-01-15 12:18:16 -0300

    feat: Add encryption logic to the user model

--- todo_api/models.py
+++ todo_api/models.py
@@ -1,4 +1,5 @@
 import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from todo_api.extensions import db
 
@@ -15,6 +16,17 @@ class User(db.Model):
                             foreign_keys='Todo.user_id',
                             backref='user',
                             lazy=True)
+
+    @property
+    def unhashed_password(self):
+        raise ValueError("You cannot get the unhashed password!")
+
+    @unhashed_password.setter
+    def unhashed_password(self, unhashed_password: str):
+        self.password = generate_password_hash(unhashed_password)
+
+    def validate_password(self, unhashed_password: str) -> bool:
+        return check_password_hash(self.password, unhashed_password)
 
 
 class Todo(db.Model):
