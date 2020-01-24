cd2dc9ccab338f4ff3a2e6a05ffbb7d644810ad3
Amador Cuenca <sphi02ac@gmail.com>
2020-01-24 13:36:22 -0300

    fix: Push app context to the create_admin CLI command"

--- cli/commands/cmd_create_admin.py
+++ cli/commands/cmd_create_admin.py
@@ -1,18 +1,14 @@
-import os
-import subprocess
-
 import click
-from flask.cli import with_appcontext
 
 
+from todo_api.app import create_app
 import todo_api.services.user_service as user_service
 
 
 @click.command("create-admin")
-@with_appcontext
 @click.argument('username')
 @click.argument('password')
-def create_admin(username: str, password: str):
+def cli(username: str, password: str):
     """
     Create an admin user in the Database.
 
@@ -20,11 +16,11 @@ def create_admin(username: str, password: str):
     :param password: User password
     :return: Subprocess call result
     """
+    app = create_app()
+    app.app_context().push()
+
     if not username.strip().lower() or not password.strip().lower():
-        cmd = f"echo 'username or password is empty. Both fields are required.'"
+        click.echo("echo 'username or password is empty. Both fields are required.'")
     else:
         new_admin = user_service.create_user(username=username, unhashed_password=password, admin=True)
-
-        cmd = "Admin user created successfully." if new_admin else "Could not create the admin user."
-
-    return subprocess.call(cmd, shell=True)
+        click.echo("Admin user created successfully." if new_admin else "Could not create the admin user.")
